d2a252b8395424b5fd66818512c25cd80bcaae14
Muskan1024 <shaikhmuskan1024@gmail.com>
2026-03-09 23:00:35 +0530

    day01 practice

---
+++ practice/day01/conversation.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#This is the conversation between two friends names Tina and Rita
+
+echo "Rita – Hey Tina? Is it you?"
+
+echo "Tina – Oh Rita! How are you? It’s been a long time."
+
+echo "Rita – I am fine, what about you? Yes, we last met during the board exams."
+
+echo "Tina – I’m good too."
+
+echo "Rita – What are you doing now?"
+
+echo "Tina – Well, I have started my undergraduate studies in English Honours at St. Xaviers College in Mumbai."
+
+echo "Rita – Wow! You finally got to study the subject you loved the most in school."
+
+echo "Tina – True. What about you Rita? Wasn’t History your favourite subject?"
+
+echo "Rita – You guessed it right. I took up History Honours in Lady Shri Ram College for Women in Delhi."
+
+echo "Tina – That’s nice. I am so happy for you."
+
+echo "Rita – I am happy for you too. Let’s meet up again soon."
+
+echo "Tina – Yes, sure! We have a lot to catch up on."
+
+echo "Rita – Bye for now. I have to pick up my sister from tuition. Take care."
+
+echo "Tina – Bye, will see you soon."
